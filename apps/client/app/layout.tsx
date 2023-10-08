78ab3b1a0accb008fbfbd32e74561eb2ab455c91
Timothy Waite <tim@the-waite.com>
2023-10-08 13:15:33 -0500

    feat/add-mock-server-list

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,24 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ServerList from "@client/components/ServerList";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Harmony Chat',
-  description: 'Encrypted Messaging App',
-}
+  title: "Harmony Chat",
+  description: "Encrypted Messaging App",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="text-emerald-400 bg-slate-700">
+        <ServerList />
         {children}
       </body>
     </html>
-  )
+  );
 }
