6c63bc0d170f3041246b941fdc777c32e2263ea6
Timothy Waite <tim@the-waite.com>
2023-10-08 14:15:26 -0500

    feat/server-channel-nav

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        slate: {
+          750: '#283447',
+          775: '#212D3F'
+        }
+      },
       fontFamily: {
         'sans': ['Uni Sans', 'sans'],
       },
