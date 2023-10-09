3201a5e0bce4c73c2fe8430839cc21002dc89f90
David Waite <daveycrockett02@gmail.com>
2023-10-09 16:27:13 -0600

    chat message

--- apps/client/components/ChatMessage.tsx
+++ apps/client/components/ChatMessage.tsx
@@ -2,8 +2,10 @@
 
 export default function ChatMessage() {
     return (
-        <div>
-
-        </div>
+        <form className="flex">
+            <div className="flex flex-grow p-3">
+                <textarea className="bg-slate-750 flex flex-grow rounded p-2" placeholder="Share a message..." rows={1} />
+            </div>
+        </form>
     );
 }
